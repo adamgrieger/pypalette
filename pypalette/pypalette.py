d7a84ce5ced76c98bd48a4c78c30ea70a48fb426
Adam Grieger <adamgrieger@gmail.com>
2015-04-10 17:18:16 -0700

    Added RGB to HSV conversion

--- pypalette/pypalette.py
+++ pypalette/pypalette.py
@@ -1,10 +1,7 @@
-from PIL import Image
-
-
 def main():
-    img = Image.open("TestImages\\sunlight-through-clouds.jpg")
-    rgb_tuples = list(img.getdata())
-    print(average_color(rgb_tuples))
+    print(rgb2hsv(0, 255, 0))
+    print(rgb2hsv(192, 192, 192))
+    print(rgb2hsv(128, 128, 128))
 
 
 def average_color(rgb):
@@ -17,11 +14,38 @@ def average_color(rgb):
         g_sum += pixel[1]
         b_sum += pixel[2]
 
-    r_average = r_sum / len(rgb)
-    g_average = b_sum / len(rgb)
-    b_average = g_sum / len(rgb)
+    r_avg = r_sum / len(rgb)
+    g_avg = b_sum / len(rgb)
+    b_avg = g_sum / len(rgb)
 
-    return r_average, g_average, b_average
+    return r_avg, g_avg, b_avg
+
+
+def rgb2hsv(r, g, b):
+    rp = r / 255
+    gp = g / 255
+    bp = b / 255
+    c_max = max(rp, gp, bp)
+    c_min = min(rp, gp, bp)
+    delta = c_max - c_min
+
+    if delta == 0:
+        h = 0
+    elif c_max == rp:
+        h = 60 * (((gp - bp) / delta) % 6)
+    elif c_max == gp:
+        h = 60 * (((bp - rp) / delta) + 2)
+    else:
+        h = 60 * (((rp - gp) / delta) + 4)
+
+    if c_max == 0:
+        s = 0
+    else:
+        s = delta / c_max
+
+    v = c_max
+
+    return h, s * 100, v * 100
 
 
 if __name__ == "__main__":
