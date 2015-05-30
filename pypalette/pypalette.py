2633bbf80981a00153ad943321e78ac7cfc1d1cd
Adam Grieger <adamgrieger@gmail.com>
2015-05-29 23:15:21 -0700

    Finalized average_color()

--- pypalette/pypalette.py
+++ pypalette/pypalette.py
@@ -2,7 +2,7 @@ from statistics import mean
 from kmeans import KMeans
 
 
-def average_color(rgb):
+def average_color(rgb, prec=0):
     """Returns the average color of an image.
 
     The calculated average is just a simple arithmetic mean.
@@ -13,6 +13,8 @@ def average_color(rgb):
 
     Args:
         rgb: An image as a list of RGB tuples.
+        prec: Optional int for RGB value decimal precision,
+            defaults to 0.
 
     Returns:
         The average color as an RGB tuple.
@@ -21,4 +23,4 @@ def average_color(rgb):
     g_avg = mean([px[1] for px in rgb])
     b_avg = mean([px[2] for px in rgb])
 
-    return r_avg, g_avg, b_avg
+    return round(r_avg, prec), round(g_avg, prec), round(b_avg, prec)
